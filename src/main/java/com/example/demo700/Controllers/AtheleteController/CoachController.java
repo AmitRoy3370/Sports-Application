6f5a42c4d69333a799ce8cb35488752f98221782
AmitRoy3370 <70816613+AmitRoy3370@users.noreply.github.com>
2026-01-17 13:28:45 +0600

    Updated files before pulling latest changes

--- src/main/java/com/example/demo700/Controllers/AtheleteController/CoachController.java
+++ src/main/java/com/example/demo700/Controllers/AtheleteController/CoachController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -130,8 +131,8 @@ public class CoachController {
 		
 	}
 	
-	@GetMapping("/findByClassification")
-	public ResponseEntity<?> findByCoachClassification(@RequestParam AthleteClassificationTypes classification) {
+	@GetMapping("/findByClassification/{classification}")
+	public ResponseEntity<?> findByCoachClassification(@PathVariable AthleteClassificationTypes classification) {
 		
 		try {
 			
