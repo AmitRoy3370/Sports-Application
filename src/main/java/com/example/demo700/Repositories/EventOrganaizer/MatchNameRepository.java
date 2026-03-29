61440e325bcf838f68465adafb09eaa5e93b1f86
AmitRoy3370 <70816613+AmitRoy3370@users.noreply.github.com>
2026-03-29 10:20:01 +0600

    Updated files before pulling latest changes

--- src/main/java/com/example/demo700/Repositories/EventOrganaizer/MatchNameRepository.java
+++ src/main/java/com/example/demo700/Repositories/EventOrganaizer/MatchNameRepository.java
@@ -3,6 +3,7 @@ package com.example.demo700.Repositories.EventOrganaizer;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.example.demo700.Models.EventOrganaizer.MatchName;
@@ -13,6 +14,8 @@ public interface MatchNameRepository extends MongoRepository<MatchName, String> 
 	public MatchName findByNameIgnoreCase(String name);
 	public MatchName findByMatchId(String matchId);
 	public List<MatchName> findByNameContainingIgnoreCase(String name);
-	public List<MatchName> findAllByMatchIdIn(List<String> matchId);
+
+	@Query("{ 'matchId': { $in: ?0 } }")
+	public List<MatchName> findByMatchIdIn(List<String> matchId);
 	
 }
