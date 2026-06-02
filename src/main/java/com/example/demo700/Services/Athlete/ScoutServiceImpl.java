80a479c4f326b048417b21dd64de39dd7eddbdef
AmitRoy3370 <70816613+AmitRoy3370@users.noreply.github.com>
2026-06-02 16:04:18 +0600

    Updated files before pulling latest changes

--- src/main/java/com/example/demo700/Services/Athlete/ScoutServiceImpl.java
+++ src/main/java/com/example/demo700/Services/Athlete/ScoutServiceImpl.java
@@ -326,7 +326,7 @@ public class ScoutServiceImpl implements ScoutService {
 
 						}
 
-						if (team.getScouts().equals(scout.getId())) {
+						if (team.getScouts().contains(scout.getId())) {
 
 							find = true;
 							break;
